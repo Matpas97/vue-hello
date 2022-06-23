8e7dac8e393eeb90922b3b63cbbc31c170f855f5
Matpas97 <passarellim22@gmail.com>
2022-06-23 19:00:38 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data (in data avrò l'src).
+
+// Dichiaro la variabile
+var app = new Vue({
+    el: '#root',
+    data: {
+        message:'Ciao javascript Vue'
+    }
+
+});
